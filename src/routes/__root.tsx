b9e999a0fa37f1a2d4effa06ec8cb9f56fc12a0b
toya <96932308+toyaAoi@users.noreply.github.com>
2026-02-11 07:13:33 +0530

    feat: show login modal only on protected routes

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,12 +1,11 @@
 import { Outlet, createRootRoute } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
-import { AuthModalProvider } from "@/hooks/useAuthModal";
 
 export const Route = createRootRoute({
   component: () => (
-    <AuthModalProvider>
+    <>
       <Outlet />
       {import.meta.env.DEV && <TanStackRouterDevtools />}
-    </AuthModalProvider>
+    </>
   ),
 });
