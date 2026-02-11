11d0b3862f4db624ce1158f3e2612b7d8d7bff9a
toya <96932308+toyaAoi@users.noreply.github.com>
2026-02-11 07:13:33 +0530

    feat: show login modal only on protected routes

--- src/routes/_app.tsx
+++ src/routes/_app.tsx
@@ -2,6 +2,7 @@ import { createFileRoute, Outlet } from "@tanstack/react-router";
 import { Box } from "@mui/joy";
 import Sidebar from "@/components/layout/Sidebar";
 import { useSocketEvent } from "@/hooks/useSocket";
+import { AuthModalProvider } from "@/hooks/useAuthModal";
 
 export const Route = createFileRoute("/_app")({
   component: RouteComponent,
@@ -13,26 +14,28 @@ function RouteComponent() {
   });
 
   return (
-    <Box
-      sx={{
-        display: "flex",
-        minHeight: "100vh",
-        backgroundColor: "background.body",
-      }}
-    >
-      <Sidebar />
-
+    <AuthModalProvider>
       <Box
-        component="main"
         sx={{
-          flexGrow: 1,
-          backgroundColor: "#fafafa",
-          height: "100vh",
-          overflowY: "auto",
+          display: "flex",
+          minHeight: "100vh",
+          backgroundColor: "background.body",
         }}
       >
-        <Outlet />
+        <Sidebar />
+
+        <Box
+          component="main"
+          sx={{
+            flexGrow: 1,
+            backgroundColor: "#fafafa",
+            height: "100vh",
+            overflowY: "auto",
+          }}
+        >
+          <Outlet />
+        </Box>
       </Box>
-    </Box>
+    </AuthModalProvider>
   );
 }
